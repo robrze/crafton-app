0edddf8f1cce2fc10d3fb12f893b59cbae14609e
Robert Rzepnikowski <robert.rzepnikowski@briisk.co>
2019-09-13 13:09:51 +0200

    add for-who section to central page view; add local fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.scss";
 import { Header } from "./components/header/header";
+import { CentralPageView } from './components/central-page-view/central-page-view';
 
 const App = () => (
   <div className="application">
     <Header />
-    <div></div>
+    <CentralPageView />
   </div>
 );
 
---
+++ src/components/central-page-view/central-page-view.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import globe from "../../assets/images/globe.png";
+import "./central-page-view.scss";
+
+export const CentralPageView = () => {
+  return (
+    <div className="central-view">
+      <div className="central-view__for-who-section">
+        <div>
+          <p className="central-view__title">Dla kogo?</p>
+          <p className="central-view__description">
+            Dedykowane nauczycielom posiadającym przygotowanie pedagogiczne,
+            którzy chcą uzyskać merytoryczne i metodyczne przygotowanie do
+            nauczania kolejnego przedmiotu - Geografii.
+            <br />
+            <br /> Studia adresowane są do osób legitymujących się wyższym
+            wykształceniem na poziomie studiów licencjackim o specjalnościach
+            nadających kwalifikacje nauczycielskie i przygotowanie pedagogiczne,
+            lub słuchaczy legitymujących się wyższym wykształceniem na poziomie
+            studiów magisterskich nadających kwalifikacje nauczycielskie i
+            przygotowanie pedagogiczne, zgodnie z Rozporządzeniem Ministra
+            Edukacji Narodowej z dnia 1 sierpnia 2017 r. w sprawie szczegółowych
+            kwalifikacji wymaganych od nauczycieli{" "}
+            <span className="central-view__link">(Dz.U. 2017 poz. 1575)</span>.
+            <br />
+            <br />
+            <p className="central-view__requirements">
+              <p className="central-view__requirement-text">
+                Wymagane od kandydata:
+              </p>
+              <p className="central-view__requirement-row">
+                <p className="central-view__arrow"> → </p>
+                <p className="central-view__requirement-text">
+                  Ukończone studia wyższe
+                </p>
+              </p>
+              <p className="central-view__requirement-row">
+                <p className="central-view__arrow"> → </p>
+                <p className="central-view__requirement-text">
+                  Przygotowanie pedagogiczne
+                </p>
+              </p>
+            </p>
+          </p>
+        </div>
+        <img className="central-view__globe" src={globe} alt="globe.png" />
+      </div>
+    </div>
+  );
+};
+
+export default CentralPageView;
