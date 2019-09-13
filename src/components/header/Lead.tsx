c7b05630873f0f6506e2c9f1582b412f68dcf092
Robert Rzepnikowski <robert.rzepnikowski@briisk.co>
2019-09-13 14:19:16 +0200

    refactoring; add header panel background

--- src/components/header/Lead.tsx
+++ src/components/header/Lead.tsx
@@ -6,10 +6,10 @@ export const Lead = () => (
     <div className="lead__content">
       <span className="lead__text">
         Będziesz uczyć się z ciekawością. <br />
-        <span className="lead__text--blue">Obiecujemy.</span>
+        <span className="lead__blue-text">Obiecujemy.</span>
       </span>
       <br />
-      <div className="lead__actions">
+      <div>
         <Button onClick={() => {}}>ZOBACZ WIĘCEJ</Button>
         <Button transparent onClick={() => {}}>
           UMÓW SIĘ Z ANIOŁEM
