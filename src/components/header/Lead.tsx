d8e3a7e0c234bb9aa58d33b8e22cef8cfb79ccdc
Robert Rzepnikowski <robert.rzepnikowski@briisk.co>
2019-09-20 16:13:21 +0200

    add header bullets

--- src/components/header/Lead.tsx
+++ src/components/header/Lead.tsx
@@ -1,20 +1,30 @@
 import React from "react";
 import { Button } from "../button/button";
 
+const VerticalBullets = () => (
+  <div className="bullets">
+    <span className="bullet">•</span>
+    <span className=" bullet bullet--yellow">•</span>
+    <span className="bullet">•</span>
+    <span className="bullet">•</span>
+  </div>
+);
+
 export const Lead = () => (
   <div className="lead">
     <div className="lead__content">
-      <span className="lead__text">
+      <p className="lead__text">
         Będziesz uczyć się z ciekawością. <br />
         <span className="lead__blue-text">Obiecujemy.</span>
-      </span>
+      </p>
       <br />
       <div>
         <Button onClick={() => {}}>ZOBACZ WIĘCEJ</Button>
-        <Button type='transparent' onClick={() => {}}>
+        <Button type="transparent" onClick={() => {}}>
           UMÓW SIĘ Z ANIOŁEM
         </Button>
       </div>
+      <VerticalBullets />
     </div>
   </div>
 );
